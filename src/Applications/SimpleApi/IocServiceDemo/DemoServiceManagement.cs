2c652677dd239835dd3757703ab94f186101db19
Lc3586 <2445624023@qq.com>
2022-01-15 13:43:53 +0800

    ~

--- src/Applications/SimpleApi/IocServiceDemo/DemoServiceManagement.cs
+++ src/Applications/SimpleApi/IocServiceDemo/DemoServiceManagement.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using Microservice.Library.Extension;
+using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using Microservice.Library.Extension;
 
 namespace IocServiceDemo
 {
